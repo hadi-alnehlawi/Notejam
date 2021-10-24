97c5e6f21950efff4b04ba793c27541f2cb37517
hadi <hadi.alnehlawi@gmail.com>
2021-10-24 11:23:42 -0300

    param tf

--- infrastrcure/main.tf
+++ infrastrcure/main.tf
@@ -2,6 +2,12 @@ variable "env" {
   default = "production"
 }
 
+variable "db_password" {}
+
+variable "db_username" {}
+
+variable "db_name" {}
+
 variable "project" {
   default = "Notejam"
 }
@@ -11,6 +17,7 @@ variable "aws_region" {}
 variable "aws_cred_file" {}
 
 variable "aws_profile" {}
+
 resource "random_integer" "id" {
   min     = 100000
   max     = 999999
@@ -23,146 +30,56 @@ provider "aws" {
 }
 
 
+module "db" {
+  source               = "terraform-aws-modules/rds/aws"
+  version              = "~> 3.0"
+  identifier           = "notejam"
+  engine               = "postgres"
+  engine_version       = "11.10-R1"
+  family               = "postgres11" # DB parameter group
+  major_engine_version = "11"         # DB option group
+  instance_class       = "db.t3.small"
+  allocated_storage    = 5
+  storage_encrypted    = false
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  port                 = "5432"
 
-# module "db" {
-#   source               = "terraform-aws-modules/rds/aws"
-#   version              = "~> 3.0"
-#   identifier           = "notejam"
-#   engine               = "postgres"
-#   engine_version       = "11.10-R1"
-#   family               = "postgres11" # DB parameter group
-#   major_engine_version = "11"         # DB option group
-#   instance_class       = "db.t3.small"
-#   allocated_storage    = 5
-#   storage_encrypted    = false
-#   name                 = "${db_name}"
-#   username             = "${db_username}"
-#   password             = "${db_password}"
-#   port                 = "5432"
+  iam_database_authentication_enabled = true
 
-#   iam_database_authentication_enabled = true
+  vpc_security_group_ids = ["sg-${var.env}-${random_integer.id.result}"]
 
-#   vpc_security_group_ids = ["sg-${env}-${random_integer.id}"]
+  maintenance_window   = "Mon:00:00-Mon:03:00"
+  backup_window        = "03:00-06:00"
 
-#   maintenance_window   = "Mon:00:00-Mon:03:00"
-#   backup_window        = "03:00-06:00"
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval  = "30"
+  monitoring_role_name = "MyRDSMonitoringRole"
+  create_monitoring_role = true
 
-#   # Enhanced Monitoring - see example for details on how to create the role
-#   # by yourself, in case you don't want to create it automatically
-#   monitoring_interval  = "30"
-#   monitoring_role_name = "MyRDSMonitoringRole"
-#   create_monitoring_role = true
+  tags = {
+    project = var.project
+  }
 
-#   tags = {
-#     project = "${project}"
-#   }
-
-#   # DB subnet group
-#   subnet_ids = ["subnet1-${env}-${random_integer.id}", "subnet2-${env}-${random_integer.id}"]
-
-
-#   # Database Deletion Protection
-#   deletion_protection     = false
-#   backup_retention_period = 0
-#   skip_final_snapshot     = true
+  # DB subnet group
+  subnet_ids = ["subnet1-${var.env}-${random_integer.id.result}", "subnet2-${var.env}-${random_integer.id.result}"]
 
 
-#   parameters = [
-#     {
-#       name  = "character_set_client"
-#       value = "utf8mb4"
-#     },
-#     {
-#       name  = "character_set_server"
-#       value = "utf8mb4"
-#     }
-#   ]
+  # Database Deletion Protection
+  deletion_protection     = false
+  backup_retention_period = 0
+  skip_final_snapshot     = true
 
-#   options = [
-#     {
-#       option_name     = "MARIADB_AUDIT_PLUGIN"
-
-#       option_settings = [
-#         {
-#           name  = "SERVER_AUDIT_EVENTS"
-#           value = "CONNECT"
-#         },
-#         {
-#           name  = "SERVER_AUDIT_FILE_ROTATIONS"
-#           value = "37"
-#         },
-#       ]
-#     },
-#   ]
-# }
-# module "db" {
-#   source               = "terraform-aws-modules/rds/aws"
-#   version              = "~> 3.0"
-#   identifier           = "notejam"
-#   engine               = "postgres"
-#   engine_version       = "11.10-R1"
-#   family               = "postgres11" # DB parameter group
-#   major_engine_version = "11"         # DB option group
-#   instance_class       = "db.t3.small"
-#   allocated_storage    = 5
-#   storage_encrypted    = false
-#   name                 = "${db_name}"
-#   username             = "${db_username}"
-#   password             = "${db_password}"
-#   port                 = "5432"
-
-#   iam_database_authentication_enabled = true
-
-#   vpc_security_group_ids = ["sg-${env}-${random_integer.id}"]
-
-#   maintenance_window   = "Mon:00:00-Mon:03:00"
-#   backup_window        = "03:00-06:00"
-
-#   # Enhanced Monitoring - see example for details on how to create the role
-#   # by yourself, in case you don't want to create it automatically
-#   monitoring_interval  = "30"
-#   monitoring_role_name = "MyRDSMonitoringRole"
-#   create_monitoring_role = true
-
-#   tags = {
-#     project = "${project}"
-#   }
-
-#   # DB subnet group
-#   subnet_ids = ["subnet1-${env}-${random_integer.id}", "subnet2-${env}-${random_integer.id}"]
-
-
-#   # Database Deletion Protection
-#   deletion_protection     = false
-#   backup_retention_period = 0
-#   skip_final_snapshot     = true
-
-
-#   parameters = [
-#     {
-#       name  = "character_set_client"
-#       value = "utf8mb4"
-#     },
-#     {
-#       name  = "character_set_server"
-#       value = "utf8mb4"
-#     }
-#   ]
-
-#   options = [
-#     {
-#       option_name     = "MARIADB_AUDIT_PLUGIN"
-
-#       option_settings = [
-#         {
-#           name  = "SERVER_AUDIT_EVENTS"
-#           value = "CONNECT"
-#         },
-#         {
-#           name  = "SERVER_AUDIT_FILE_ROTATIONS"
-#           value = "37"
-#         },
-#       ]
-#     },
-#   ]
-# }
+  parameters = [
+      {
+        name  = "autovacuum"
+        value = 1
+      },
+      {
+        name  = "client_encoding"
+        value = "utf8"
+      }
+    ]
+}
