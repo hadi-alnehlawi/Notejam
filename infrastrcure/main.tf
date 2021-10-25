6ffa709bf62a2635ab4961cd888c7493e56fbed9
hadi <hadi.alnehlawi@gmail.com>
2021-10-24 21:19:23 -0300

    rds tf moudle

--- infrastrcure/main.tf
+++ infrastrcure/main.tf
@@ -1,6 +1,4 @@
-variable "env" {
-  default = "production"
-}
+variable "env" { default = "production"}
 
 variable "db_password" {}
 
@@ -8,20 +6,28 @@ variable "db_username" {}
 
 variable "db_name" {}
 
-variable "project" {
-  default = "Notejam"
-}
+variable "project" { default = "Notejam" }
 
-variable "aws_region" {}
+variable "vpc_name" {}
 
+variable "sg_name" {}
+
+variable "vpc_cidr"{ default="10.0.0.0/16" }
+
+variable "public_subnets" { default=["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]}
+
+variable "private_subnets" { default=["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]}
+
+variable "database_subnets" { default=["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]}
+
+variable "aws_region" { default = "sa-east-1" }
+
+variable "aws_azs" { default = ["sa-east-1a", "sa-east-1b", "sa-east-1c"] }
 variable "aws_cred_file" {}
 
 variable "aws_profile" {}
 
-resource "random_integer" "id" {
-  min     = 100000
-  max     = 999999
-}
+resource "random_uuid" "uuid" {}
 
 provider "aws" {
   region                  = var.aws_region
@@ -29,13 +35,41 @@ provider "aws" {
   profile                 = var.aws_profile
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+  azs  = var.aws_azs
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  database_subnets = var.database_subnets
+}
 
-module "db" {
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4"
+  name        = var.sg_name
+  description = "Complete PostgreSQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+}
+
+module "rds" {
   source               = "terraform-aws-modules/rds/aws"
   version              = "~> 3.0"
-  identifier           = "notejam"
+  identifier           = "notejamdbinstance"
   engine               = "postgres"
-  engine_version       = "11.10-R1"
+  engine_version       = "11.10"
   family               = "postgres11" # DB parameter group
   major_engine_version = "11"         # DB option group
   instance_class       = "db.t3.small"
@@ -48,8 +82,8 @@ module "db" {
 
   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = ["sg-${var.env}-${random_integer.id.result}"]
-
+  # vpc_security_group_ids = ["sg-${var.env}-${random_uuid.uuid.result}"]
+  vpc_security_group_ids = [module.security_group.security_group_id]
   maintenance_window   = "Mon:00:00-Mon:03:00"
   backup_window        = "03:00-06:00"
 
@@ -64,8 +98,8 @@ module "db" {
   }
 
   # DB subnet group
-  subnet_ids = ["subnet1-${var.env}-${random_integer.id.result}", "subnet2-${var.env}-${random_integer.id.result}"]
-
+  # subnet_ids = ["subnet1-${var.env}-${random_uuid.uuid.result}", "subnet2-${var.env}-${random_uuid.uuid.result}"]
+  subnet_ids = module.vpc.database_subnets
 
   # Database Deletion Protection
   deletion_protection     = false
