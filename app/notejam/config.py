03d840f611b920845f5b2585584c810b66e06b4d
hadi <hadi.alnehlawi@gmail.com>
2021-11-07 22:31:03 -0300

    update README

--- app/notejam/config.py
+++ app/notejam/config.py
@@ -7,7 +7,12 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     WTF_CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    postgres_url = os.environ.get("DB_URI")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+    postgres_url =f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     print(postgres_url)
     SQLALCHEMY_DATABASE_URI = postgres_url
 
